7410f21423b44959f24607b52dead5fc1e847ce1
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2024-01-27 19:41:18 +0530

    First Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Vivek Singh Mehta on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
