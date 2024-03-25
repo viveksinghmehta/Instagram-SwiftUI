8abc3464a17f160963727c3570b1fa3d360e477a
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2024-03-25 19:00:47 +0530

    Added Post Model

---
+++ Instagram/Model/PostModel.swift
@@ -0,0 +1,29 @@
+//
+//  PostModel.swift
+//  Instagram
+//
+//  Created by Vivek Singh Mehta on 24/03/24.
+//
+
+import Foundation
+
+struct PostModel: Identifiable, Hashable, Codable {
+    let id: String
+    let ownerUid: String
+    var caption: String?
+    var likes: Int
+    let imageUrl: String
+    let timeStamp: Date
+    var user: UserModel?
+}
+
+extension PostModel {
+    static let MOCK_POSTS: [PostModel] = [
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 23, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[3]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 53, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[2]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 13, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[1]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 23, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[3]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 29, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[0]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: nil, likes: 234, imageUrl: ImageUrl.getRandomImage(), timeStamp: Date(), user: UserModel.Mock_Users[2])
+    ]
+}
